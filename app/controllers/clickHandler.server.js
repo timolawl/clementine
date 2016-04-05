15cc21abe5b07d781465cd35e833eb1f969a67b9
Timolawl <timolawl@users.noreply.github.com>
2016-04-04 20:25:17 -0700

    Finish clementine tutorial

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -29,6 +29,33 @@ function clickHandler (db) {
             }
         });
     };
+
+    this.addClick = function (req, res) {
+        clicks
+            .findAndModify(
+                    {},
+                    { '_id': 1 },
+                    { $inc: { 'clicks': 1 } },
+                    function (err, result) {
+                        if (err) { throw err; }
+
+                        res.json(result);
+                    }
+            );
+    };
+
+    this.resetClicks = function (req, res) {
+        clicks
+            .update(
+                    {},
+                    { 'clicks': 0 },
+                    function (err, result) {
+                        if (err) { throw err; }
+
+                        res.json(result);
+                    }
+            );
+    };
 }
 
 module.exports = clickHandler;
